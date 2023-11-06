8f9ee68fac5bcae130fd00408e36e0c1f9bd2cb0
ArmaanChaand <armaan.chaand17@gmail.com>
2023-11-06 18:59:54 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+function App() {
+
+  return (
+    <>
+      <div className="logo-name--container">
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/black-bespoke-logo.svg" className="logo" alt="Vite logo" />
+        </a>
+        <h1>Black Bespoke</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
